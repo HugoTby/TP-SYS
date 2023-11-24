432e80377217c0389fce3c8d9b499299119cb19b
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-11-24 09:47:31 +0100

    Update main.cpp

--- ClochesADLink/ClochesADLink/main.cpp
+++ ClochesADLink/ClochesADLink/main.cpp
@@ -1,3 +1,16 @@
+//*********************************************************************************************
+//* Programme : main.cpp                                                    Date : 24/11/2023
+//*--------------------------------------------------------------------------------------------
+//* Dernière mise à jour : 24/11/2023
+//*
+//* Programmeurs : Alexandre Lepretre                                         Classe : BTSSN2
+//*                Hugo Tabary
+//*                Edouard Hautemanière
+//*--------------------------------------------------------------------------------------------
+//* But : C'est le point d'entrée principal de l'application
+//* Programmes associés : ClochesADLink.h, QtWidgets/QApplication
+//*********************************************************************************************
+
 #include "ClochesADLink.h"
 #include <QtWidgets/QApplication>
 
